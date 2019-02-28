9ffc9ed83670c4068e1fcd41cf16ba3916848df1
marlonsouza <marlon.souuza@gmail.com>
2019-02-28 00:13:19 -0300

    Bootcamp NodeJS Modulo 1

---
+++ src/app/models/User.js
@@ -0,0 +1,30 @@
+const bcryptjs = require('bcryptjs')
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcryptjs.hash(user.password, 8)
+          }
+        }
+      }
+    }
+  )
+
+  User.prototype.checkPassword = function (password) {
+    return bcryptjs.compare(password, this.password_hash)
+  }
+
+  return User
+}
